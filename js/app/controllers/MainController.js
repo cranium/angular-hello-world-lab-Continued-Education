790388393150e8af983a212ac3464d771234db7e
Carson Crane <carson@ccrane.me>
2016-02-28 19:29:42 -0500

    finished

---
+++ js/app/controllers/MainController.js
@@ -0,0 +1,9 @@
+var app = angular.module("app");
+
+app.controller("MainController", ['$scope', function($scope) {
+	$scope.name = "Jenny";
+
+	$scope.email = "notarealmail@gmoiaofasdf.biz";
+
+	$scope.phone = 8675309;
+}]);
